05277f2ee7d1e1d5228eeae1077c4ceb259c60ca
Alexander Obuhovich <aik.bold@gmail.com>
2014-11-11 14:06:30 +0200

    Moving Jenkins-related config options from Diffusion to Harbormaster

--- src/applications/harbormaster/config/PhabricatorHarbormasterConfigOptions.php
+++ src/applications/harbormaster/config/PhabricatorHarbormasterConfigOptions.php
@@ -12,7 +12,15 @@ final class PhabricatorHarbormasterConfigOptions
   }
 
   public function getOptions() {
-    return array();
+    return array(
+      $this->newOption('jenkins.host', 'string', null)
+        ->setDescription(pht('Jenkins installation hostname.')),
+      $this->newOption('jenkins.user-id', 'string', null)
+        ->setDescription(pht('Username for accessing Jenkins.')),
+      $this->newOption('jenkins.api-token', 'string', null)
+        ->setMasked(true)
+        ->setDescription(pht('API token for accessing Jenkins.')),
+    );
   }
 
 }
