b902ea5e565c27f83c7b0bfd224a15394bba4ba7
Alexander Obuhovich <aik.bold@gmail.com>
2015-02-13 23:19:51 +0200

    Rename constant whitespace mode constant for commits
    Related to "T3498-audit-whitespace-ignore" branch, but can't put it in there because "WHITESPACE_IGNORE_ALL" constant was added only to master recently (see d66cbff2981108afd9276a18c3906b722a533b4d).

--- src/applications/diffusion/controller/DiffusionCommitController.php
+++ src/applications/diffusion/controller/DiffusionCommitController.php
@@ -274,11 +274,11 @@ final class DiffusionCommitController extends DiffusionController {
         DifferentialChangesetParser::WHITESPACE_SHOW_ALL);
 
       $options = array(
-        DifferentialChangesetParser::WHITESPACE_IGNORE_FORCE => 'Ignore All',
-        DifferentialChangesetParser::WHITESPACE_IGNORE_ALL => 'Ignore Most',
+        DifferentialChangesetParser::WHITESPACE_IGNORE_ALL => pht('Ignore All'),
+        DifferentialChangesetParser::WHITESPACE_IGNORE_MOST => pht('Ignore Most'),
         DifferentialChangesetParser::WHITESPACE_IGNORE_TRAILING =>
-          'Ignore Trailing',
-        DifferentialChangesetParser::WHITESPACE_SHOW_ALL => 'Show All',
+          pht('Ignore Trailing'),
+        DifferentialChangesetParser::WHITESPACE_SHOW_ALL => pht('Show All'),
       );
 
       foreach ($options as $value => $label) {
