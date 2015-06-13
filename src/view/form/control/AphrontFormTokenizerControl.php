4a3dc2a96bdb51a4c4c840f15ed1e2133530a780
Alexander Obuhovich <aik.bold@gmail.com>
2015-06-13 15:16:31 +0300

    The "AphrontFormTokenizerControl" control can read value from request

--- src/view/form/control/AphrontFormTokenizerControl.php
+++ src/view/form/control/AphrontFormTokenizerControl.php
@@ -38,6 +38,11 @@ final class AphrontFormTokenizerControl extends AphrontFormControl {
     $this->loadHandles();
   }
 
+  public function readValueFromRequest(AphrontRequest $request) {
+    $this->setValue($request->getArr($this->getName()));
+    return $this;
+  }
+
   protected function renderInput() {
     $name = $this->getName();
 
